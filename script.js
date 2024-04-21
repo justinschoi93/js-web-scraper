352924690b85cb0d18d73d53a87d65a22a4d537e
jOwl <jhome@js-MacBook-Pro.local>
2024-04-20 17:20:04 -0700

    I will turn this into an ig reel scraper

--- script.js
+++ script.js
@@ -3,8 +3,16 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const playwright = require("playwright");
 const result = {};
+
+import inquirer from "inquirer";
 // GET USER INPUT
 
+const url = inquirer.prompt("Enter the URL of the website you want to scrape:").then(response => console.log(response));
+
+  inquirer.prompt("Enter the URL of the website you want to scrape:")
+    .then(response => {
+      return response;
+    });
 
 // FETCH DATA
 async function scrapeData() {
@@ -25,5 +33,19 @@ async function scrapeData() {
 // DISPLAY DATA
 
 
+///////// WORK SPACE //////////
 
+const extractData = $ => { // This function is unique to each webpage. In this case, it extracts all elements with the class value of product
+  $('.product').map((_, product)=>{
+    const $product  = $(product)
 
+    return (
+      {
+        id: $product.find('a[data-product_id]'), // finds <a> with attribute data-product_id
+        title: $product.find('h2').text(),
+        price: $product.find('.price').text(),
+      }
+    )
+  })
+  .toArray(); // Converts cheerio array to jQuery array;
+}
